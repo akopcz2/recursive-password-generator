fcf64a5b3eadd419cc9b85fba7bad58ba5f0a091
Adrian Kopczewski <adrian.kopczewski@designory.com>
2018-07-27 13:20:56 -0500

    Updating npm module ref call

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let randomPass = require('./pw.js');
+let randomPass = require('wordlist-password-generator');
 let userPassword = new randomPass(5,2);
 userPassword.init().then((value) => {
     const userPassword = value;
