59dc45acd7accc2f1e9f55c974114ce093aaf3b1
akopcz2 <akopcz2@gmail.com>
2018-07-28 10:47:08 -0500

    Updating comments

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-let randomPass = require('wordlist-password-generator');
+let randomPass = require('./pw.js');
 /** 
- * (5) Number of letters you want to word to have
+ * (4) Number of letters you want to word to have
  * (2) Levels of depth - 2 Yield 2 words and 2 numbers
  * */
-let userPassword = new randomPass(5,2);
+let userPassword = new randomPass(4,2);
 userPassword.init().then((value) => {
     const userPassword = value;
     console.log(userPassword);
